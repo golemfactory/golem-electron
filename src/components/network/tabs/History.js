240fecc05b0e2483258f3da8283d5d23f77e5ae7
Muhammed Tanrıkulu <md.tanrikulu@gmail.com>
2017-06-27 10:35:41 +0200

    History date preview and sort correction

--- src/components/network/tabs/History.js
+++ src/components/network/tabs/History.js
@@ -23,27 +23,49 @@ export class History extends React.Component {
      * [loadHistory loading payment history as DOM]
      */
     loadHistory() {
+        function isDateToday(date) {
+            let today = new Date();
+            let givenDay = new Date(date.replace(/(\d{2})\/(\d{2})\/(\d{4})/, "$2/$1/$3"));
+            return (today.toDateString() == givenDay.toDateString());
+        }
+
         function timeStampToHR(timestamp) {
+            console.log("timestamp", timestamp);
             // Create a new JavaScript Date object based on the timestamp
             // multiplied by 1000 so that the argument is in milliseconds, not seconds.
-            var date = new Date(timestamp);
+            var date = new Date(Number(timestamp));
             console.log("date", date);
+            var days = "0" + date.getDate();
+            var month = "0" + (date.getMonth() + 1); //January is 0!
+            var year = date.getFullYear();
+
+            var formattedDate = days.substr(-2) + '/' + month.substr(-2) + '/' + year;
             // Hours part from the timestamp
-            var hours = date.getHours();
+            var hours = "0" + date.getHours();
             // Minutes part from the timestamp
             var minutes = "0" + date.getMinutes();
             // Seconds part from the timestamp
             var seconds = "0" + date.getSeconds();
 
             // Will display time in 10:30:23 format
-            var formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
-            return formattedTime
+            var formattedTime = hours.substr(-2) + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+            return `${isDateToday(formattedDate) ? '' : formattedDate + ' -'} ${formattedTime}`
         }
+
+        function newestToOldest(a, b) {
+            if (a.created < b.created)
+                return 1;
+            if (a.created > b.created)
+                return -1;
+            return 0;
+        }
+        ;
+
         const {historyList} = this.props
-        return (historyList.map(({payee, payer, created, status, value, type, transaction}, index) => <div key={index} className="item__history">
+        return (historyList.sort(newestToOldest).map(({payee, payer, created, status, value, type, transaction}, index) => <div key={index} className="item__history">
             <div className="info__history">
                 <h5>{(payee || payer).substr(0, 32)}...</h5>
-                <span>{timeStampToHR(created)}</span>
+                <span>{timeStampToHR((created * (10 ** 3)).toFixed(0))}</span>
                 <span className="status__history">{status}</span>
             </div>
             <div className="action__history">
