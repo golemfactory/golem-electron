0eb77516a91524f4377a079b2fad0cde8cf8a457
Muhammed Tanrıkulu <md.tanrikulu@gmail.com>
2017-08-17 15:06:56 +0200

    Unique key for array children issue solved.

--- src/components/tasks/TaskDetail.js
+++ src/components/tasks/TaskDetail.js
@@ -522,14 +522,14 @@ export class TaskDetail extends React.Component {
             {
                 order: 0,
                 detailContent: false,
-                content: <div className="item-settings">
+                content: <div className="item-settings" key="0">
                             <span className="title">Preset</span>
                             <Dropdown list={presetList} handleChange={this._handlePresetOptionChange.bind(this, presetList)} disabled={isDetailPage} manageHandler={::this._handleManagePresetModal}  presetManager/> 
                         </div>
             },
             {
                 order: 1,
-                content: <div className="item-settings">
+                content: <div className="item-settings" key="1">
                                 <span className="title">Dimensions</span>
                                 <input ref="resolutionW" type="number" min="100" max="8000" aria-label="Dimension (width)" onChange={this._handleResolution.bind(this, 0)} required={!isDetailPage} disabled={isDetailPage}/>
                                 <span className="icon-cross"/>
@@ -538,14 +538,14 @@ export class TaskDetail extends React.Component {
             },
             {
                 order: 3,
-                content: <div className="item-settings">
+                content: <div className="item-settings" key="3">
                                 <span className="title">Format</span>
                                 <Dropdown ref="formatRef" list={mockFormatList} selected={formatIndex} handleChange={this._handleFormatOptionChange.bind(this, mockFormatList)} disabled={isDetailPage}/> 
                          </div>
             },
             {
                 order: 4,
-                content: <div className="item-settings">
+                content: <div className="item-settings" key="4">
                                 <span className="title">Output to</span>
                                 <input ref="outputPath" type="text" placeholder="…Docs/Golem/Output" aria-label="Output path" disabled/>
                                 <button type="button" className="btn--outline" onClick={::this._handleOutputPath} disabled={isDetailPage}>Change</button>
@@ -554,27 +554,27 @@ export class TaskDetail extends React.Component {
             {
                 order: 6,
                 detailContent: false,
-                content: <div className="item-settings item__preset-button">
+                content: <div className="item-settings item__preset-button" key="6">
                                 <button type="button" className="btn--outline" onClick={::this._handleSavePresetModal} disabled={savePresetLock}>Save as preset</button>
                             </div>
             },
             {
                 order: 7,
-                content: <div className="item-settings">
+                content: <div className="item-settings" key="7">
                                 <span className="title">Task Timeout</span>
                                 <input ref="taskTimeout" type="text" aria-label="Task Timeout" onKeyDown={this._handleTimeoutInputs.bind(this, 'timeout')} required={!isDetailPage} disabled={isDetailPage}/>
                             </div>
             },
             {
                 order: 8,
-                content: <div className="item-settings">
+                content: <div className="item-settings" key="8">
                                 <span className="title">Subtask Amount</span>
                                 <input ref="subtaskCount" type="number" min="1" max="100" placeholder="8" aria-label="Subtask amount" onChange={this._handleFormInputs.bind(this, 'subtasks')} required={!isDetailPage} disabled={isDetailPage}/>
                             </div>
             },
             {
                 order: 9,
-                content: <div className="item-settings">
+                content: <div className="item-settings" key="9">
                                 <span className="title">Subtask Timeout</span>
                                 <input ref="subtaskTimeout" type="text" aria-label="Deadline" onKeyDown={this._handleTimeoutInputs.bind(this, 'subtask_timeout')} required={!isDetailPage} disabled={isDetailPage}/>
                             </div>
@@ -585,14 +585,14 @@ export class TaskDetail extends React.Component {
         case taskType.BLENDER:
             formTemplate.push({
                 order: 2,
-                content: <div className="item-settings">
+                content: <div className="item-settings" key="2">
                             <span className="title">Frame Range</span>
                             <input ref="framesRef" type="text" aria-label="Frame Range" pattern="^[0-9]?(([0-9\s;,-]*)[0-9])$" onChange={this._handleFormInputs.bind(this, 'frames')} required={!isDetailPage} disabled={isDetailPage}/>
                          </div>
             })
             formTemplate.push({
                 order: 5,
-                content: <div className="item-settings">
+                content: <div className="item-settings" key="5">
                             <span className="title">Blender Compositing</span>
                             <div className="switch-box switch-box--green">
                                 <span>{compositing ? 'On' : 'Off'}</span>
@@ -607,7 +607,7 @@ export class TaskDetail extends React.Component {
         case taskType.LUXRENDER:
             formTemplate.push({
                 order: 5,
-                content: <div className="item-settings">
+                content: <div className="item-settings" key="5">
                             <span className="title">Sample per pixel</span>
                             <input ref="haltspp" type="text" placeholder="1" min="1" max="2000" aria-label="Sample per pixel" onChange={this._handleFormInputs.bind(this, 'sample_per_pixel')} required={!isDetailPage} disabled={isDetailPage}/>
                          </div>
