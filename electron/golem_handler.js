6d199f9201e61d9c528bc56231ded4fa15217ca6
Marek Franciszkiewicz <marek.franciszkiewicz@imapp.pl>
2017-10-23 18:40:11 +0200

    Revert "golemapp version checking (#72)"
    This reverts commit 931535624a2b0e52dc0f934e953f05f116a4de09.

--- electron/golem_handler.js
+++ electron/golem_handler.js
@@ -4,11 +4,10 @@ const os = require('os');
 const path = require('path');
 const log = require('./debug_handler.js')
 
-const {execSync, spawn, spawnSync} = require('child_process');
+const {exec, execSync, spawn} = require('child_process');
 const {app} = electron;
 
 const WHITESPACE_REGEXP = /\s*[\s,]\s*/;
-const MINIMUM_GOLEM_VERSION = [0, 7, 1];
 
 
 class GolemProcess {
@@ -19,17 +18,12 @@ class GolemProcess {
         this.processArgs = processArgs || ['-r', '127.0.0.1:61000'];
     }
 
-    startProcess() {
-        try {
-            if (this.process) return;
-            if (!this.checkVersion()) return;
+    startProcess(err, pid) {
+        if (!this.process)
             this._startProcess();
-        } catch (e) {
-            console.error('GolemProcess error:', e);
-        }
     }
 
-    prepareSpawnOptions() {
+    _startProcess() {
         let cwd = path.join(os.homedir(), '.golem');
         let env = process.env;
         let platform = os.platform();
@@ -53,54 +47,8 @@ class GolemProcess {
         this.process = spawn(this.processName, this.processArgs, {
             cwd: cwd,
             env: env,
-        }
-    }
-
-    checkVersion() {
-        let options = this.prepareSpawnOptions();
-        options.stdio = 'pipe';
-        options.timeout = 1000; // ms
-        options.killSignal = 'SIGKILL';
-        let result = spawnSync(this.processName, ['--version'], options);
-        if (result.error) {
-            if (result.error.code == 'ENOENT') {
-                console.error(this.processName,
-                        "not found! Make sure it's in your $PATH");
-                return false;
-            } else {
-                console.error(this.processName, "failed. ERRNO:",
-                        result.error.errno, "code:", result.error.code);
-                return false;
-            }
-        }
-        if (result.status != 0) {
-            console.error('Cannot start Golem process. Exit code:',
-                    result.status,
-                    'stderr:',
-                    result.stderr && result.stderr.toString() || "<null>"
-            );
-            return false;
-        }
-        let version_re = /^GOLEM version: (\d+\.\d+\.\d+)$/m;
-        let version_match = version_re.exec(result.stdout.toString());
-        let process_version = version_match[1].split(".");
-        if (process_version < MINIMUM_GOLEM_VERSION) {
-            console.error(this.processName,
-                    ' version is', process_version.join(".")
-                    +'. Minimum required version is',
-                    MINIMUM_GOLEM_VERSION.join(".") + '. Please upgrade.'
-            );
-            return false;
-        }
-        return true;
-    }
-
-    _startProcess() {
-        let options = this.prepareSpawnOptions();
-        options.stdio = 'ignore';
-
-        console.log('💻 Starting Golem...');
-        this.process = spawn(this.processName, this.processArgs, options);
+            stdio: 'ignore'
+        });
 
         /* Handle process events */
         this.process.on('error', data => {
