9a61340d1732a74d90c796a000935cc46af15664
Muhammed Tanrıkulu <md.tanrikulu@gmail.com>
2017-10-24 18:39:52 +0200

    Stats wording

--- src/components/settings/Stats.js
+++ src/components/settings/Stats.js
@@ -23,8 +23,8 @@ export class Stats extends React.Component {
         const {isEngineOn, stats} = this.props
         return (
             <div className="content__stats">
-                <p>Network:{stats.in_network}</p>
-                <p>Supported: {stats.supported}</p>
+                <p>Tasks on network:{stats.in_network}</p>
+                <p>Supported tasks: {stats.supported}</p>
                 <p>Computed subtask: {stats.subtasks_computed[1]}</p>
                 <p>Subtask with errors: {stats.subtasks_with_errors[1]}</p>
                 <p>Subtask with timeouts: {stats.subtasks_with_timeout[1]}</p>
