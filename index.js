26845bf14e828ac2ea8e6679398d626ff7fa6896
Muhammed Tanrikulu <md.tanrikulu@gmail.com>
2017-12-28 19:10:07 +0100

    Windows resource path problem solved

--- index.js
+++ index.js
@@ -257,131 +257,135 @@ function createPreviewWindow(id, frameCount) {
     })
 }
 
+function isWin(){
+    return process.platform === "win32"
+}
+
 
 exports.selectDirectory = function(directory) {
-console.log("directory", directory);
+    console.log("directory", directory);
 
-let blackList = [
-    "ACTION",
-    "APK",
-    "APP",
-    "BAT",
-    "BIN",
-    "CMD",
-    "COM",
-    "COMMAND",
-    "CPL",
-    "CSH",
-    "EXE",
-    "GADGET",
-    "INF",
-    "INS",
-    "INX",
-    "IPA",
-    "ISU",
-    "JOB",
-    "JSE",
-    "KSH",
-    "LNK",
-    "MSC",
-    "MSI",
-    "MSP",
-    "MST",
-    "OSX",
-    "OUT",
-    "PAF",
-    "PIF",
-    "PRG",
-    "REG",
-    "RGS",
-    "RUN",
-    "SCR",
-    "SCT",
-    "SHB",
-    "SHS",
-    "U3B",
-    "VB",
-    "VBE",
-    "VBS",
-    "VBSCRIPT",
-    "WORKFLOW",
-    "WS",
-    "WSF",
-    "WSH",
-]
+    let blackList = [
+        "ACTION",
+        "APK",
+        "APP",
+        "BAT",
+        "BIN",
+        "CMD",
+        "COM",
+        "COMMAND",
+        "CPL",
+        "CSH",
+        "EXE",
+        "GADGET",
+        "INF",
+        "INS",
+        "INX",
+        "IPA",
+        "ISU",
+        "JOB",
+        "JSE",
+        "KSH",
+        "LNK",
+        "MSC",
+        "MSI",
+        "MSP",
+        "MST",
+        "OSX",
+        "OUT",
+        "PAF",
+        "PIF",
+        "PRG",
+        "REG",
+        "RGS",
+        "RUN",
+        "SCR",
+        "SCT",
+        "SHB",
+        "SHS",
+        "U3B",
+        "VB",
+        "VBE",
+        "VBS",
+        "VBSCRIPT",
+        "WORKFLOW",
+        "WS",
+        "WSF",
+        "WSH",
+    ]
 
-const masterList = [
-    "BLEND",
-    "LXS"
-]
+    const masterList = [
+        "BLEND",
+        "LXS"
+    ]
 
-let ignorePlaftormFiles = function(file) {
-    return path.basename(file) !== ".DS_Store" && path.extname(file) !== null
-}
-
-let isBadFile = function(file) {
-    let correctExtension = file.replace(".", "").toUpperCase()
-    return blackList.includes(correctExtension)
-}
-
-let isMasterFile = function(file) {
-    let correctExtension = file.replace(".", "").toUpperCase()
-    return masterList.includes(correctExtension)
-}
-
-let walk = function(dir, done) {
-    let results = [];
-    fs.readdir(dir, function(err, list) {
-        if (err) return done(err);
-        let i = 0;
-        (function next() {
-            let file = list[i++];
-            if (!file) return done(null, results);
-            file = `${dir}/${file}`;
-            fs.stat(file, function(err, stat) {
-                if (stat && stat.isDirectory()) {
-                    walk(file, function(err, res) {
-                        results = results.concat(res);
-                        next();
-                    });
-                } else {
-                    ignorePlaftormFiles(file) && results.push({
-                        path: file,
-                        name: path.basename(file),
-                        extension: path.extname(file),
-                        malicious: isBadFile(path.extname(file)),
-                        master: isMasterFile(path.extname(file))
-                    });
-                    next();
-                }
-            });
-        })();
-    });
-};
-
-let promises = directory.length > 0 && directory.map(item => new Promise((resolve, reject) => {
-    if (fs.lstatSync(item).isDirectory())
-        walk(item, function(err, results) {
-            if (err) {
-                reject(err);
-            }
-            resolve(results);
-        });
-    else {
-        let results = [];
-        ignorePlaftormFiles(item) && results.push({
-            path: item,
-            name: path.basename(item),
-            extension: path.extname(item),
-            malicious: isBadFile(path.extname(item)),
-            master: isMasterFile(path.extname(item))
-        });
-        resolve(results)
+    let ignorePlaftormFiles = function(file) {
+        return path.basename(file) !== ".DS_Store" && path.extname(file) !== null
     }
-}))
 
-win.focus();
-return Promise.all(promises)
+    let isBadFile = function(file) {
+        let correctExtension = file.replace(".", "").toUpperCase()
+        return blackList.includes(correctExtension)
+    }
+
+    let isMasterFile = function(file) {
+        let correctExtension = file.replace(".", "").toUpperCase()
+        return masterList.includes(correctExtension)
+    }
+
+    let walk = function(dir, done) {
+        let results = [];
+        fs.readdir(dir, function(err, list) {
+            if (err) return done(err);
+            let i = 0;
+            (function next() {
+                let file = list[i++];
+                if (!file) return done(null, results);
+                file = isWin() ? `${dir}\\\\${file}` : `${dir}/${file}`;
+                fs.stat(file, function(err, stat) {
+                    if (stat && stat.isDirectory()) {
+                        walk(file, function(err, res) {
+                            results = results.concat(res);
+                            next();
+                        });
+                    } else {
+                        ignorePlaftormFiles(file) && results.push({
+                            path: file,
+                            name: path.basename(file),
+                            extension: path.extname(file),
+                            malicious: isBadFile(path.extname(file)),
+                            master: isMasterFile(path.extname(file))
+                        });
+                        next();
+                    }
+                });
+            })();
+        });
+    };
+
+    let promises = directory.length > 0 && directory.map(item => new Promise((resolve, reject) => {
+        if (fs.lstatSync(item).isDirectory())
+            walk(item, function(err, results) {
+                if (err) {
+                    reject(err);
+                }
+                resolve(results);
+            });
+        else {
+            let results = [];
+            ignorePlaftormFiles(item) && results.push({
+                path: item,
+                name: path.basename(item),
+                extension: path.extname(item),
+                malicious: isBadFile(path.extname(item)),
+                master: isMasterFile(path.extname(item))
+            });
+            resolve(results)
+        }
+    }))
+
+    win.focus();
+    return Promise.all(promises)
 }
 
 
@@ -391,8 +395,7 @@ function createLocationPath(_dir){
 
 exports.getDefaultLocation = function() {
 
-    const isWin = process.platform === "win32"
-    const _location = isWin ? `${process.env.USERPROFILE}\\Documents` : `${process.env.HOME}/Documents`;
+    const _location = isWin() ? `${process.env.USERPROFILE}\\Documents` : `${process.env.HOME}/Documents`;
 
     if (!fs.existsSync(_location))
         return createLocationPath(_location)
