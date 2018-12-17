5ebc0beb1c4b16d87db3495a0a333247849779bf
Muhammed Tanrikulu <md.tanrikulu@gmail.com>
2018-12-17 15:00:56 +0100

    Concent soft switch RPC implemented

--- src/sagas/concent.js
+++ src/sagas/concent.js
@@ -1,10 +1,34 @@
-import { takeLatest, call, put } from 'redux-saga/effects'
+import { fork, takeLatest, call, put } from 'redux-saga/effects'
 import { dict } from '../actions'
 
 import { config, _handleRPC } from './handler'
 
 
 const {TOGGLE_CONCENT, UNLOCK_CONCENT_DEPOSIT, SET_CONCENT_SWITCH} = dict
+
+export function fectConcentStatus(session) {
+    return new Promise((response, reject) => {
+
+        function on_info(args) {
+            let info = args[0];
+            response({
+                type: SET_CONCENT_SWITCH,
+                info
+            })
+        }
+        
+        _handleRPC(on_info, session, config.CONCENT_SWITCH_STATUS_RPC)
+    })
+}
+
+/**
+ * [*toggleConcentUnlockBase generator to unlock concent deposit]
+ * @param {[type]} session       [Session of the wamp connection]
+ */
+export function* fetchConcentStatusBase(session) {
+    const action = yield call(fectConcentStatus, session);
+    //yield action && put(action)
+}
 
 export function unlockDepositConcent(session) {
     return new Promise((response, reject) => {
@@ -31,34 +55,35 @@ export function toggleConcent(session, {payload, informRPC, toggleLock = false})
     return new Promise((response, reject) => {
 
         if(!informRPC)
+
             response({
                 type: SET_CONCENT_SWITCH,
                 payload
             })
+
         else {
-            response({
-                type: SET_CONCENT_SWITCH,
-                payload
-            })
+            
+            function on_info(args) {
+                let info = args[0]
+                console.log("info", info)
+                response({
+                    type: SET_CONCENT_SWITCH,
+                    payload: info
+                })
+            }
+
+            function on_lock(args) {
+                let lock = args[0];
+                console.log("lock", lock);
+                _handleRPC(on_info, session, CONCENT_SWITCH_RPC, [payload])
+            }
+            
+            _handleRPC(on_lock, session, toggleLock ? config.CONCENT_UNLOCK : config.CONCENT_RELOCK)
+
+
 
             console.log("RPC informed")
         }
-        // function on_info(args) {
-        //     let info = args[0]
-        //     console.log("info", info)
-        //     response({
-        //         type: SET_CONCENT_SWITCH,
-        //         payload: info
-        //     })
-        // }
-
-        // function on_lock(args) {
-        //     let lock = args[0];
-        //     console.log("lock", lock);
-        //     _handleRPC(on_info, session, payload ? config.CONCENT_ON_RPC : config.CONCENT_OFF_RPC)
-        // }
-        
-        // _handleRPC(on_lock, session, toggleLock ? config.CONCENT_UNLOCK : config.CONCENT_RELOCK)
     })
 }
 
@@ -72,6 +97,7 @@ export function* toggleConcentBase(session, payload) {
 }
 
 export function* concentFlow(session, payload) {
+    yield fork(fetchConcentStatusBase, session)
     yield takeLatest(TOGGLE_CONCENT, toggleConcentBase, session)
     yield takeLatest(UNLOCK_CONCENT_DEPOSIT, toggleConcentUnlockBase, session)
 }
