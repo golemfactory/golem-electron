c7cb1d9a490708e61c7dc567a84a1d46a9820f29
Muhammed Tanrikulu <md.tanrikulu@gmail.com>
2019-01-16 17:41:08 +0100

    Loader bar fix

--- src/components/LoaderBar.js
+++ src/components/LoaderBar.js
@@ -13,44 +13,36 @@ export default class LoaderBar extends Component {
     }
 
     componentWillUnmount() {
-        var id = window.setTimeout(null,0);
-        while (id--) 
-        {   
-            window.clearTimeout(id);
-        }
+        this.loaderBarInterval && clearInterval(this.loaderBarInterval)
     }
 
 
     _initLoaderBar(){
         const loadingSpanList = document.getElementsByClassName("loading"),
               steps        = 20,     // length of bar
-              speed        = 50,     // #ms
-              iteration    = 2000;  // iteration * speed = timeout #ms
+              speed        = 50;     // #ms
 
         let c = 0,
-            p = Promise.resolve();
+            p = Promise.resolve(),
+            i = 0;
 
-        for (let i = 0; i < iteration; i++) {
-            p = p.then(_ => new Promise(resolve =>
-                setTimeout(function () {
-                        parseInt(i/steps) % 2 == 0 
+        this.loaderBarInterval = setInterval(() => {
+            parseInt(i/steps) % 2 == 0 
                         ? c++
                         : c--;
-                    drawLoader(c, resolve);
-                }, speed)
-            ));
-        }
+            drawLoader(c);
+            i++;
+        }, speed);
 
         function drawLoader(i, resolve){
             const loadArray = new Array(steps + 5).fill("-")
-            loadArray[0] = "[";
-            loadArray[steps + 4] = "]";
-            loadArray[i+1] = "="
-            loadArray[i+2] = "="
-            loadArray[i+3] = "="
+            loadArray[0]          = "[";
+            loadArray[steps + 4]  = "]";
+            loadArray[i+1]        = "="
+            loadArray[i+2]        = "="
+            loadArray[i+3]        = "="
             Array.from(loadingSpanList)
             .forEach(item => item.textContent = loadArray.join(""));
-            resolve();
          }
     }
 
