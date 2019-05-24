d6598fa7f890dfb33b82e72bc28cede36442feb9
Muhammed Tanrikulu <md.tanrikulu@gmail.com>
2019-05-24 17:51:27 +0200

    Missing amount modal redesigned

--- src/components/tasks/modal/InsufficientAmountModal.js
+++ src/components/tasks/modal/InsufficientAmountModal.js
@@ -1,4 +1,19 @@
 import React from "react";
+import { BigNumber } from 'bignumber.js';
+import Lottie from "react-lottie";
+
+import animData from "./../../../assets/anims/warning";
+
+const ETH_DENOM = 10 ** 18;
+
+const defaultOptions = {
+    loop: false,
+    autoplay: true,
+    animationData: animData,
+    rendererSettings: {
+        preserveAspectRatio: "xMidYMid slice"
+    }
+};
 
 export default class InsufficientAmountModal extends React.Component {
     constructor(props) {
@@ -16,24 +31,38 @@ export default class InsufficientAmountModal extends React.Component {
 
     render() {
         const { message } = this.props;
+        const { error_details, error_msg, error_type } = message;
         return (
             <div className="container__modal container__task-error-modal">
                 <div className="content__modal">
-                    <div>
-                        <span className="icon-warning" />
+                    <div className="image-container">
+                        <Lottie options={defaultOptions} />
                     </div>
                     <span className="info">
-                        You <strong>cannot</strong> send this task to the
+                        You don't have enough funds to send this task to the
                         network.
                     </span>
-                    <pre className="message-from-server">{message}</pre>
+                    <div>
+                        {error_details &&
+                        error_details?.missing_funds
+                            .map( item => 
+                            <div className="amount__panel">
+                                <div>
+                                    <span className="amount__missing">{new BigNumber(item.required).dividedBy(ETH_DENOM).toFixed(4)} {item.currency}</span>
+                                </div>
+                                <div>
+                                    <center>Available:</center>
+                                    <span className="amount__available"><b>{new BigNumber(item.available).dividedBy(ETH_DENOM).toFixed(8)}</b> {item.currency}</span>
+                                </div>
+                            </div>)}
+                    </div>
                     <div className="action__modal">
                         <span
                             className="btn--cancel"
                             onClick={this._handleCancel}>
                             Cancel
                         </span>
-                        {message.includes("Not enough") ? (
+                        {error_type === "NotEnoughFunds" ? (
                             <button
                                 type="button"
                                 className="btn--primary"
