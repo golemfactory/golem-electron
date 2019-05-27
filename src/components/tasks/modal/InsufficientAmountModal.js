e00188000d6e79cfe6ec61a9106057d83c10d7af
Muhammed Tanrikulu <md.tanrikulu@gmail.com>
2019-05-27 17:48:30 +0200

    Mockup of insufficient deposit modal on task creation implemented

--- src/components/tasks/modal/InsufficientAmountModal.js
+++ src/components/tasks/modal/InsufficientAmountModal.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BigNumber } from 'bignumber.js';
 import Lottie from 'react-lottie';
+import Tooltip from '@tippy.js/react';
 
 import animData from './../../../assets/anims/warning';
 
@@ -18,6 +19,11 @@ const defaultOptions = {
 export default class InsufficientAmountModal extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            withConcent: false
+        }
+
+        this._initContent = this._initContent.bind(this);
     }
 
     /**
@@ -29,72 +35,148 @@ export default class InsufficientAmountModal extends React.Component {
 
     _handleRetry = () => window.routerHistory.push('/tasks');
 
+    _handleStart = () => console.info('created!');
+
+    _handleConcentCheckbox = e => this.setState({withConcent: e.target.value});
+
+    _initContent(message) {
+        const { error_details, error_msg, error_type } = message;
+
+        const createFundsInfo = (missing_funds) => {
+            const result = [
+                <div className="amount__panel" key="amountPanel">
+                    {missing_funds.map(item => (
+                        <div className="amount__item" key={item.currency}>
+                            <div>
+                                <span className="amount__missing">
+                                    {new BigNumber(item.required)
+                                        .dividedBy(ETH_DENOM)
+                                        .toFixed(4)}{' '}
+                                    {item.currency}
+                                </span>
+                            </div>
+                            <div>
+                                <center>Available:</center>
+                                <span className="amount__available">
+                                    <b>
+                                        {new BigNumber(item.available)
+                                            .dividedBy(ETH_DENOM)
+                                            .toFixed(8)}
+                                    </b>{' '}
+                                    {item.currency}
+                                </span>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            ];
+            if(error_type === 'NotEnoughDepositFunds')
+                result.push(
+                    <div
+                        className="radio-group"
+                        onChange={this._handleConcentCheckbox}
+                        key="concentRadioGroup">
+                        <div className="radio-item">
+                            <input
+                                type="radio"
+                                id="createConcentRadio"
+                                value={true}
+                                name="createConcentRadio"
+                                defaultChecked={true}
+                            />
+                            <label htmlFor="createConcentRadio">
+                                <span className="overlay" />
+                                Top up account and finish with Concent
+                                <span className="icon-question-mark" />
+                            </label>
+                        </div>
+                        <div className="radio-item">
+                            <input
+                                type="radio"
+                                id="createConcentRadio2"
+                                value={false}
+                                name="createConcentRadio"
+                            />
+                            <label htmlFor="createConcentRadio2">
+                                <span className="overlay" />
+                                Compute this task without concent
+                                <span className="icon-question-mark" />
+                            </label>
+                        </div>
+                    </div>
+                    )
+            return result
+        }
+
+        switch (error_type) {
+            case 'NotEnoughFunds':
+                return {
+                    title:
+                        "You don't have enough funds to send this task to the network.",
+                    content: createFundsInfo(error_details?.missing_funds),
+                    actionButton: (
+                        <button
+                            type="button"
+                            className="btn--primary"
+                            onClick={this._handleTopUp}
+                            autoFocus>
+                            Top up
+                        </button>
+                    )
+                };
+            case 'NotEnoughDepositFunds':
+                return {
+                    title:
+                        "You don't have enough deposit to create this task with concent.",
+                    content: createFundsInfo(error_details?.missing_funds),
+                    actionButton: (
+                        <button
+                            type="button"
+                            className="btn--primary"
+                            onClick={this._handleStart}
+                            autoFocus>
+                            Apply
+                        </button>
+                    )
+                };
+            default:
+                return {
+                    title: 'You cannot send this task to the network.',
+                    content: (
+                        <pre className="message-from-server">{message}</pre>
+                    ),
+                    actionButton: (
+                        <button
+                            type="button"
+                            className="btn--primary"
+                            onClick={this._handleRetry}
+                            autoFocus>
+                            Retry
+                        </button>
+                    )
+                };
+        }
+    }
+
     render() {
         const { message } = this.props;
-        const { error_details, error_msg, error_type } = message;
-        
+        const { title, content, actionButton } = this._initContent(message);
+
         return (
             <div className="container__modal container__task-error-modal">
                 <div className="content__modal">
                     <div className="image-container">
                         <Lottie options={defaultOptions} />
                     </div>
-                    <span className="info">
-                        You {error_details ? "don't have enough funds to" : "cannot"} send this task to the
-                        network.
-                    </span>
-                    <div className="amount__panel">
-                        {error_details ? (
-                            error_details?.missing_funds.map(item => (
-                                <div className="amount__item" key={item.currency}>
-                                    <div>
-                                        <span className="amount__missing">
-                                            {new BigNumber(item.required)
-                                                .dividedBy(ETH_DENOM)
-                                                .toFixed(4)}{' '}
-                                            {item.currency}
-                                        </span>
-                                    </div>
-                                    <div>
-                                        <center>Available:</center>
-                                        <span className="amount__available">
-                                            <b>
-                                                {new BigNumber(item.available)
-                                                    .dividedBy(ETH_DENOM)
-                                                    .toFixed(8)}
-                                            </b>{' '}
-                                            {item.currency}
-                                        </span>
-                                    </div>
-                                </div>
-                            ))
-                        ) : (
-                            <pre className="message-from-server">{message}</pre>
-                        )}
-                    </div>
+                    <span className="info">{title}</span>
+                    {content}
                     <div className="action__modal">
                         <span
                             className="btn--cancel"
                             onClick={this._handleCancel}>
                             Cancel
                         </span>
-                        {error_type === 'NotEnoughFunds' ? (
-                            <button
-                                type="button"
-                                className="btn--primary"
-                                onClick={this._handleTopUp}
-                                autoFocus>
-                                Top up
-                            </button>
-                        ) : (
-                            <button
-                                type="button"
-                                className="btn--primary"
-                                onClick={this._handleRetry}
-                                autoFocus>
-                                Retry
-                            </button>
-                        )}
+                        {actionButton}
                     </div>
                 </div>
             </div>
