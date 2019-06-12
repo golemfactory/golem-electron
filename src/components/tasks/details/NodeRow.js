71888b415deb9df9e2a9eecfe26b2a4dd91512b6
Muhammed Tanrikulu <md.tanrikulu@gmail.com>
2019-06-12 03:18:02 +0200

    Copying node & job id implemented on node list

--- src/components/tasks/details/NodeRow.js
+++ src/components/tasks/details/NodeRow.js
@@ -1,24 +1,75 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import Tooltip from '@tippy.js/react';
 
-const { ipcRenderer } = window.electron;
+const { ipcRenderer, clipboard } = window.electron;
 
 const NodeRow = ({ item, showBlockNodeModal }) => {
+	const [ isDataCopied, setCopyStatus ] = useState(false);
+	let copyTimeout;
+
+	useEffect(() => {
+		return () => {
+			copyTimeout && clearTimeout(copyTimeout)
+		}
+	}, [])
+
 	const _handleOpenFile = (path, parentFolder) =>
 		ipcRenderer.send('open-file', path, parentFolder);
 
 	const _showBlockNodeModal = subtask => showBlockNodeModal(subtask);
 
+	const _copyField = item => {
+		if (item) {
+            clipboard.writeText(item);
+                	
+            setCopyStatus(true);
+            copyTimeout = setTimeout(() => {
+                setCopyStatus(false);
+            }, 3000);
+        }
+	}
+
 	return (
 		<tr className="fragment__node-row">
 			<td align="center">
-				{item.subtask_id.replace(
-					new RegExp('^(.{0,12}).*(.{2})$', 'im'),
-					'$1...$2'
-				)}
+				<Tooltip
+                    content={
+                        <p>
+                            {isDataCopied
+                                ? 'Copied Succesfully!'
+                                : 'Click to copy ID'}
+                        </p>
+                    }
+                    placement="top"
+                    trigger="mouseenter"
+                    hideOnClick={false}>
+						<span className="id-info" onClick={_copyField.bind(null, item.subtask_id)}>
+							{item.subtask_id.replace(
+								new RegExp('^(.{0,12}).*(.{2})$', 'im'),
+								'$1...$2'
+							)}
+						</span>
+					</Tooltip>
 			</td>
 			<td align="center">{item.status}</td>
-			<td align="center">{item.node_name || "Anonymous"}</td>
+			<td align="center">
+				<Tooltip
+                    content={
+                        <p>
+                            {isDataCopied
+                                ? 'Copied Succesfully!'
+                                : 'Click to copy ID'}
+                        </p>
+                    }
+                    placement="top"
+                    trigger="mouseenter"
+                    hideOnClick={false}>
+						<span className="id-info" onClick={_copyField.bind(null, item.node_id)}>
+							{item.node_name || "Anonymous"}
+						</span>
+					</Tooltip>
+			</td>
 			<td align="center">
 				<span
 					className="icon-logs"
