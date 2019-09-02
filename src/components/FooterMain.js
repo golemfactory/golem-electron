9fac3b9a0aeb36f4f4d659a61dde0340828a8772
Muhammed Tanrikulu <md.tanrikulu@gmail.com>
2019-09-02 15:15:53 +0200

    Graceful shutdown RPC connected

--- src/components/FooterMain.js
+++ src/components/FooterMain.js
@@ -87,7 +87,7 @@ const mapStateToProps = state => ({
     componentWarnings: getComponentWarnings(state, 'componentWarnings'),
     chosenPreset: state.advanced.chosenPreset,
     isEngineOn: state.info.isEngineOn,
-    stats: state.stats.stats.provider,
+    stats: state.stats.stats.provider || state.stats.stats,
     isEngineLoading: state.info.isEngineLoading,
     version: state.info.version
 });
--- src/sagas/quit.js
+++ src/sagas/quit.js
@@ -3,7 +3,24 @@ import { dict } from '../actions'
 
 import { config, _handleRPC } from './handler'
 
-const {APP_QUIT} = dict
+const {APP_QUIT, APP_QUIT_GRACEFUL} = dict
+
+export function gracefulShutdown(session, cb) {
+        function on_app(args) {
+            let appStatus = args[0];
+            cb(appStatus)
+        }
+
+        _handleRPC(on_app, session, config.QUIT_GRACEFUL_RPC)
+}
+
+/**
+ * [*terminateGolemBase generator terminate golem core]
+ * @param {[type]} session       [Session of the wamp connection]
+ */
+export function* gracefulShutdownBase(session, {_cb}) {
+    yield call(gracefulShutdown, session, _cb);
+}
 
 export function terminateGolem(session, cb) {
         function on_app(args) {
@@ -29,4 +46,5 @@ export function* terminateGolemBase(session, {_cb}) {
  */
 export function* quitFlow(session) {
     yield takeLatest(APP_QUIT, terminateGolemBase, session)
+    yield takeLatest(APP_QUIT_GRACEFUL, gracefulShutdownBase, session)
 }
